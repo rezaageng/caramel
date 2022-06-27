c0839e3e31cad202264e4994d8fb2b09ef7d3161
Reza Ageng <rezaageng5@gmail.com>
2022-06-27 12:02:37 +0700

    chore(config): :rotating_light: fix eslint conflict with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -12,7 +17,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }],
     'import/extensions': [
@@ -25,6 +30,7 @@ module.exports = {
         tsx: 'never',
       },
     ],
+    'prettier/prettier': 'error',
   },
   settings: {
     'import/resolver': {
